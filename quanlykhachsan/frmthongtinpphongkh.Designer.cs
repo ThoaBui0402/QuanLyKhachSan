123c09927d338544609e61757314091dd35dcd1b
DoThiDiep <cactusivyyj@gmail.com>
2021-11-17 22:55:40 +0700

    Form TT Phòng

--- quanlykhachsan/frmthongtinpphongkh.Designer.cs
+++ quanlykhachsan/frmthongtinpphongkh.Designer.cs
@@ -28,123 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.maPhong = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.loaiPhong = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.tinhTrang = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.donGia = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.label5 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.thoat = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+               this.dataGridView1 = new System.Windows.Forms.DataGridView();
+               this.maPhong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.loaiPhong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.tinhTrang = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.donGia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.label5 = new System.Windows.Forms.Label();
+               this.button1 = new System.Windows.Forms.Button();
+               this.thoat = new System.Windows.Forms.Button();
+               ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+               this.SuspendLayout();
+               // 
+               // dataGridView1
+               // 
+               this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+               this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+               this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+               this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.maPhong,
             this.loaiPhong,
             this.tinhTrang,
             this.donGia});
-            this.dataGridView1.Location = new System.Drawing.Point(1, 100);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(1029, 434);
-            this.dataGridView1.TabIndex = 16;
-            // 
-            // maPhong
-            // 
-            this.maPhong.DataPropertyName = "maPhong";
-            this.maPhong.HeaderText = "Phòng";
-            this.maPhong.MinimumWidth = 6;
-            this.maPhong.Name = "maPhong";
-            this.maPhong.ReadOnly = true;
-            // 
-            // loaiPhong
-            // 
-            this.loaiPhong.DataPropertyName = "loaiPhong";
-            this.loaiPhong.HeaderText = "Loại Phòng";
-            this.loaiPhong.MinimumWidth = 6;
-            this.loaiPhong.Name = "loaiPhong";
-            this.loaiPhong.ReadOnly = true;
-            // 
-            // tinhTrang
-            // 
-            this.tinhTrang.DataPropertyName = "tinhTrang";
-            this.tinhTrang.HeaderText = "Tình Trạng";
-            this.tinhTrang.MinimumWidth = 6;
-            this.tinhTrang.Name = "tinhTrang";
-            this.tinhTrang.ReadOnly = true;
-            // 
-            // donGia
-            // 
-            this.donGia.DataPropertyName = "donGia";
-            this.donGia.HeaderText = "Đơn Giá";
-            this.donGia.MinimumWidth = 6;
-            this.donGia.Name = "donGia";
-            this.donGia.ReadOnly = true;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label5.ForeColor = System.Drawing.Color.Red;
-            this.label5.Location = new System.Drawing.Point(352, 42);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(345, 38);
-            this.label5.TabIndex = 15;
-            this.label5.Text = "THÔNG TIN PHÒNG";
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.button1.Location = new System.Drawing.Point(252, 544);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(136, 49);
-            this.button1.TabIndex = 17;
-            this.button1.Text = "Đăng xuất";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
-            // thoat
-            // 
-            this.thoat.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.thoat.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.thoat.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.thoat.Location = new System.Drawing.Point(597, 544);
-            this.thoat.Margin = new System.Windows.Forms.Padding(4);
-            this.thoat.Name = "thoat";
-            this.thoat.Size = new System.Drawing.Size(136, 49);
-            this.thoat.TabIndex = 18;
-            this.thoat.Text = "Thoát";
-            this.thoat.UseVisualStyleBackColor = false;
-            this.thoat.Click += new System.EventHandler(this.thoat_Click);
-            // 
-            // frmthongtinpphongkh
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1029, 608);
-            this.Controls.Add(this.thoat);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.label5);
-            this.Margin = new System.Windows.Forms.Padding(4);
-            this.Name = "frmthongtinpphongkh";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "frmthongtinpphongkh";
-            this.Load += new System.EventHandler(this.frm_load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+               this.dataGridView1.Location = new System.Drawing.Point(1, 100);
+               this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+               this.dataGridView1.Name = "dataGridView1";
+               this.dataGridView1.ReadOnly = true;
+               this.dataGridView1.RowHeadersWidth = 51;
+               this.dataGridView1.Size = new System.Drawing.Size(1029, 434);
+               this.dataGridView1.TabIndex = 16;
+               // 
+               // maPhong
+               // 
+               this.maPhong.DataPropertyName = "maPhong";
+               this.maPhong.HeaderText = "Phòng";
+               this.maPhong.MinimumWidth = 6;
+               this.maPhong.Name = "maPhong";
+               this.maPhong.ReadOnly = true;
+               // 
+               // loaiPhong
+               // 
+               this.loaiPhong.DataPropertyName = "loaiPhong";
+               this.loaiPhong.HeaderText = "Loại Phòng";
+               this.loaiPhong.MinimumWidth = 6;
+               this.loaiPhong.Name = "loaiPhong";
+               this.loaiPhong.ReadOnly = true;
+               // 
+               // tinhTrang
+               // 
+               this.tinhTrang.DataPropertyName = "tinhTrang";
+               this.tinhTrang.HeaderText = "Tình Trạng";
+               this.tinhTrang.MinimumWidth = 6;
+               this.tinhTrang.Name = "tinhTrang";
+               this.tinhTrang.ReadOnly = true;
+               // 
+               // donGia
+               // 
+               this.donGia.DataPropertyName = "donGia";
+               this.donGia.HeaderText = "Đơn Giá";
+               this.donGia.MinimumWidth = 6;
+               this.donGia.Name = "donGia";
+               this.donGia.ReadOnly = true;
+               // 
+               // label5
+               // 
+               this.label5.AutoSize = true;
+               this.label5.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+               this.label5.ForeColor = System.Drawing.Color.Red;
+               this.label5.Location = new System.Drawing.Point(352, 42);
+               this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+               this.label5.Name = "label5";
+               this.label5.Size = new System.Drawing.Size(345, 38);
+               this.label5.TabIndex = 15;
+               this.label5.Text = "THÔNG TIN PHÒNG";
+               // 
+               // button1
+               // 
+               this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+               this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+               this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+               this.button1.Location = new System.Drawing.Point(252, 544);
+               this.button1.Margin = new System.Windows.Forms.Padding(4);
+               this.button1.Name = "button1";
+               this.button1.Size = new System.Drawing.Size(136, 49);
+               this.button1.TabIndex = 17;
+               this.button1.Text = "Đăng xuất";
+               this.button1.UseVisualStyleBackColor = false;
+               // 
+               // thoat
+               // 
+               this.thoat.BackColor = System.Drawing.SystemColors.ActiveCaption;
+               this.thoat.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+               this.thoat.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+               this.thoat.Location = new System.Drawing.Point(597, 544);
+               this.thoat.Margin = new System.Windows.Forms.Padding(4);
+               this.thoat.Name = "thoat";
+               this.thoat.Size = new System.Drawing.Size(136, 49);
+               this.thoat.TabIndex = 18;
+               this.thoat.Text = "Thoát";
+               this.thoat.UseVisualStyleBackColor = false;
+               this.thoat.Click += new System.EventHandler(this.thoat_Click);
+               // 
+               // frmthongtinpphongkh
+               // 
+               this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+               this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+               this.ClientSize = new System.Drawing.Size(1029, 608);
+               this.Controls.Add(this.thoat);
+               this.Controls.Add(this.button1);
+               this.Controls.Add(this.dataGridView1);
+               this.Controls.Add(this.label5);
+               this.Margin = new System.Windows.Forms.Padding(4);
+               this.Name = "frmthongtinpphongkh";
+               this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+               this.Text = "Thông tin phòng";
+               this.Load += new System.EventHandler(this.frm_load);
+               ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+               this.ResumeLayout(false);
+               this.PerformLayout();
 
         }
 
